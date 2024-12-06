46bfe851f4f6ba8515085a98cadfd1d63a96d835
Evgeny Vrublevsky <me@veg.by>
2024-12-06 15:20:47 +0200

    Add ARM64 configuration.

--- bin/install.cmd
+++ bin/install.cmd
@@ -2,6 +2,12 @@
 setlocal
 cd %~dp0
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSTYPE=32BIT || set OSTYPE=64BIT
-if %OSTYPE%==32BIT su32 cmd /C copy /B /-Y su32.exe "%WINDIR%\system32\su.exe" ^&^& pause
-if %OSTYPE%==64BIT su64 cmd /C copy /B /-Y su64.exe "%WINDIR%\system32\su.exe" ^&^& copy /B /-Y su32.exe "%WINDIR%\syswow64\su.exe" ^&^& pause
+if defined PROCESSOR_ARCHITEW6432 (
+	set ARCH=%PROCESSOR_ARCHITEW6432%
+) else (
+	set ARCH=%PROCESSOR_ARCHITECTURE%
+)
+
+if %ARCH%==x86   su32 cmd /C copy /B /-Y su32.exe "%WINDIR%\system32\su.exe" ^&^& pause
+if %ARCH%==AMD64 su64 cmd /C copy /B /-Y su64.exe "%WINDIR%\system32\su.exe" ^&^& copy /B /-Y su32.exe "%WINDIR%\syswow64\su.exe" ^&^& pause
+if %ARCH%==ARM64 su64a cmd /C copy /B /-Y su64a.exe "%WINDIR%\system32\su.exe" ^&^& copy /B /-Y su32.exe "%WINDIR%\syswow64\su.exe" ^&^& pause
--- bin/uninstall.cmd
+++ bin/uninstall.cmd
@@ -2,6 +2,12 @@
 setlocal
 cd %~dp0
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSTYPE=32BIT || set OSTYPE=64BIT
-if %OSTYPE%==32BIT su32 cmd /C del /P "%WINDIR%\system32\su.exe" ^&^& pause
-if %OSTYPE%==64BIT su64 cmd /C del /P "%WINDIR%\system32\su.exe" ^&^& del /P "%WINDIR%\syswow64\su.exe" ^&^& pause
+if defined PROCESSOR_ARCHITEW6432 (
+	set ARCH=%PROCESSOR_ARCHITEW6432%
+) else (
+	set ARCH=%PROCESSOR_ARCHITECTURE%
+)
+
+if %ARCH%==x86   su32 cmd /C del /P "%WINDIR%\system32\su.exe" ^&^& pause
+if %ARCH%==AMD64 su64 cmd /C del /P "%WINDIR%\system32\su.exe" ^&^& del /P "%WINDIR%\syswow64\su.exe" ^&^& pause
+if %ARCH%==ARM64 su64a cmd /C del /P "%WINDIR%\system32\su.exe" ^&^& del /P "%WINDIR%\syswow64\su.exe" ^&^& pause
